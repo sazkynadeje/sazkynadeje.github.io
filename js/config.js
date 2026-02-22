4972dc3df5ad6d4501a96691fe76aff8483aad6f
sazkynadeje <zbynek.kasnar@gmail.com>
2026-02-22 23:22:26 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,16 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC-NCdmsgA42FxnopKm92m1y5gUGw_z_uE", 
+    authDomain: "nadeje-208bd.firebaseapp.com", 
+    databaseURL: "https://nadeje-208bd-default-rtdb.europe-west1.firebasedatabase.app", 
+    projectId: "nadeje-208bd", 
+    storageBucket: "nadeje-208bd.firebasestorage.app", 
+    messagingSenderId: "688478977789", 
+    appId: "1:688478977789:web:512d1520cf96c9e59cc894" 
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export const db = firebase.database();
 
