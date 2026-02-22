f89901007d63349cb87698b9366d8b93a6379162
sazkynadeje <zbynek.kasnar@gmail.com>
2026-02-22 23:29:31 +0100

    Update database.js

--- js/database.js
+++ js/database.js
@@ -1,21 +1,6 @@
+import { db } from './config.js'; // Importujeme již vytvořené spojení
 import { AuthService } from './auth.js';
 import { sanitizeId } from './core.js';
-
-const firebaseConfig = {
-    apiKey: "AIzaSyC-NCdmsgA42FxnopKm92m1y5gUGw_z_uE", 
-    authDomain: "nadeje-208bd.firebaseapp.com", 
-    databaseURL: "https://nadeje-208bd-default-rtdb.europe-west1.firebasedatabase.app", 
-    projectId: "nadeje-208bd", 
-    storageBucket: "nadeje-208bd.firebasestorage.app", 
-    messagingSenderId: "688478977789", 
-    appId: "1:688478977789:web:512d1520cf96c9e59cc894" 
-};
-
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
-
-const db = firebase.database();
 
 export const DBService = {
     // Načte denní bank (převedený z minula)
